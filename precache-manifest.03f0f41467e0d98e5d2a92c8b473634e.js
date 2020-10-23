dbb540f717e9b0b3700a92aa530cefab5a81f535
Atulya Kumar <67427578+atulya-karn@users.noreply.github.com>
2020-10-23 11:38:50 +0530

    Updates

---
+++ precache-manifest.03f0f41467e0d98e5d2a92c8b473634e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210a3637f8d21e4d475f1c1c40469077",
+    "url": "/React-1/index.html"
+  },
+  {
+    "revision": "a0ac8cb38d84e6b9bcc3",
+    "url": "/React-1/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "b654bbca5d6e0612c653",
+    "url": "/React-1/static/css/main.cf1ad034.chunk.css"
+  },
+  {
+    "revision": "a0ac8cb38d84e6b9bcc3",
+    "url": "/React-1/static/js/2.96d97846.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-1/static/js/2.96d97846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b654bbca5d6e0612c653",
+    "url": "/React-1/static/js/main.f2807b51.chunk.js"
+  },
+  {
+    "revision": "380c4686143668eb3afd",
+    "url": "/React-1/static/js/runtime-main.7077ad99.js"
+  }
+]);
